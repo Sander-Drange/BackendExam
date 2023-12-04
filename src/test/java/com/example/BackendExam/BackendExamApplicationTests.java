b860f2b3bf421478b3c69bcef42b7450986d7494
Sander-Drange <sander.drange@hotmail.com>
2023-12-04 12:35:50 +0100

    initial commit

---
+++ src/test/java/com/example/BackendExam/BackendExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackendExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
