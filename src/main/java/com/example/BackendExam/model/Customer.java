ea2ee21a528d9d7fc768196978d6c24ce4774931
Sander-Drange <sadr002@student.kristiania.no>
2023-12-05 13:26:27 +0100

    added Customer classInfo

--- src/main/java/com/example/BackendExam/model/Customer.java
+++ src/main/java/com/example/BackendExam/model/Customer.java
@@ -1,8 +1,9 @@
 package com.example.BackendExam.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Generated;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
@@ -11,15 +12,43 @@ import java.time.LocalDate;
 @Table
 @Getter
 @Setter
+@NoArgsConstructor
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+
     private Long id;
     private String name;
     private String email;
     private LocalDate dob;
     private Integer age;
 
-    public Customer (){
+    public Customer(String name,
+                    String email,
+                    LocalDate dob,
+                    Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
     }
 
-
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
 }
