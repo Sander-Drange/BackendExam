ec626756cc3a858a6f08681a635f615c8e1ab95f
Sander-Drange <sadr002@student.kristiania.no>
2023-12-05 14:17:41 +0100

    added context to repos

--- src/main/java/com/example/BackendExam/repository/AddressRepository.java
+++ src/main/java/com/example/BackendExam/repository/AddressRepository.java
@@ -1,4 +1,8 @@
 package com.example.BackendExam.repository;
 
-public interface AddressRepository {
+import com.example.BackendExam.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
 }
