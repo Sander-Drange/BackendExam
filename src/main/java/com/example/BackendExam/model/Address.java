2e535f565848093b5473b7439f7cc4d4f3cdcb26
Michping <michaelhalnik@gmail.com>
2023-12-05 13:26:24 +0100

    fixed address model

--- src/main/java/com/example/BackendExam/model/Address.java
+++ src/main/java/com/example/BackendExam/model/Address.java
@@ -1,7 +1,36 @@
 package com.example.BackendExam.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
 public class Address {
+    @Id
+    @SequenceGenerator(
+            name = "address_sequence",
+            sequenceName = "address_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "address_sequence"
+    )
+    private long id;
+    private String street;
+    private int streetNumber;
+    private String city;
+    private String country;
+
+    public Address(String street, int streetNumber, String city, String country) {
+        this.street = street;
+        this.streetNumber = streetNumber;
+        this.city = city;
+        this.country = country;
+    }
 }
