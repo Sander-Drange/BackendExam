885f359c65ac1256ffa2afe3f4b3f6b1a2cd1e51
Sander-Drange <sadr002@student.kristiania.no>
2023-12-05 14:29:16 +0100

    added Service context

--- src/main/java/com/example/BackendExam/service/MachineService.java
+++ src/main/java/com/example/BackendExam/service/MachineService.java
@@ -1,4 +1,31 @@
 package com.example.BackendExam.service;
 
+import com.example.BackendExam.model.Machine;
+import com.example.BackendExam.repository.MachineRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MachineService {
+    @Autowired
+    private MachineRepository machineRepository;
+
+    public List<Machine> findAll() {
+        return machineRepository.findAll();
+    }
+
+    public Optional<Machine> findById(Long id) {
+        return machineRepository.findById(id);
+    }
+
+    public Machine save(Machine machine) {
+        return machineRepository.save(machine);
+    }
+
+    public void deleteById(Long id) {
+        machineRepository.deleteById(id);
+    }
 }
