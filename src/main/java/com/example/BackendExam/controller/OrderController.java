efcdad9a4d6afb72b1e0adcc45beea470ecbacbf
Michping <michaelhalnik@gmail.com>
2023-12-06 14:28:22 +0100

    updated ordercontroller

---
+++ src/main/java/com/example/BackendExam/controller/OrderController.java
@@ -0,0 +1,18 @@
+package com.example.BackendExam.controller;
+
+import com.example.BackendExam.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/controller")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+}
