57be4cc9bbb3122a98581b3a75b5afce6d5010ee
Michping <michaelhalnik@gmail.com>
2023-12-06 23:48:17 +0100

    added some parts to the config class

--- src/main/java/com/example/BackendExam/configuration/PartEntitiesConfig.java
+++ src/main/java/com/example/BackendExam/configuration/PartEntitiesConfig.java
@@ -1,4 +1,29 @@
 package com.example.BackendExam.configuration;
 
+import com.example.BackendExam.model.Customer;
+import com.example.BackendExam.model.PartEntities;
+import com.example.BackendExam.repository.CustomerRepository;
+import com.example.BackendExam.repository.PartEntitiesRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
 public class PartEntitiesConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(PartEntitiesRepository partEntitiesRepository) {
+        return args -> {
+            PartEntities light = new PartEntities(
+                    "light"
+            );
+
+            PartEntities headband = new PartEntities(
+                    "headband"
+            );
+
+            partEntitiesRepository.saveAll(
+                    List.of(light, headband)
+            );
+        };
+    }
 }
