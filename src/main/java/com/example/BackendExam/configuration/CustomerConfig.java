39f7cbbd4bc924c37a580617c4d5e414207676d0
Sander-Drange <sadr002@student.kristiania.no>
2023-12-07 14:12:51 +0100

    deleted

--- src/main/java/com/example/BackendExam/configuration/CustomerConfig.java
+++ src/main/java/com/example/BackendExam/configuration/CustomerConfig.java
@@ -1,18 +1,6 @@
 package com.example.BackendExam.configuration;
 
-import com.example.BackendExam.model.Address;
-import com.example.BackendExam.model.Customer;
-import com.example.BackendExam.repository.AddressRepository;
-import com.example.BackendExam.repository.CustomerRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Set;
-
-import static java.time.Month.*;
 
 @Configuration
 public class CustomerConfig {
--- src/main/java/com/example/BackendExam/configuration/MachineConfig.java
+++ src/main/java/com/example/BackendExam/configuration/MachineConfig.java
@@ -1,12 +1,6 @@
 package com.example.BackendExam.configuration;
 
-import com.example.BackendExam.model.Machine;
-import com.example.BackendExam.repository.MachineRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class MachineConfig {
