d1311312e9a45bbb6bc476f4b7148d31fb5f18b1
Michping <michaelhalnik@gmail.com>
2023-12-15 20:44:27 +0100

    added endtoend tests

---
+++ src/test/java/com/example/BackendExam/EndToEndTests/PartEntityEndToEndTest.java
@@ -0,0 +1,68 @@
+package com.example.BackendExam.EndToEndTests;
+
+import com.example.BackendExam.model.PartEntities;
+import com.example.BackendExam.repository.PartEntitiesRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PartEntityEndToEndTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PartEntitiesRepository partEntitiesRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testGetPartEntities() throws Exception {
+        List<PartEntities> mockPartEntities = List.of(new PartEntities(), new PartEntities());
+        when(partEntitiesRepository.findAll()).thenReturn(mockPartEntities);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/part-entities"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(mockPartEntities.size()));
+    }
+
+    @Test
+    public void testGetPartEntityById() throws Exception {
+        Long partEntityId = 1L;
+        PartEntities mockPartEntity = new PartEntities();
+        mockPartEntity.setId(partEntityId);
+        when(partEntitiesRepository.findById(partEntityId)).thenReturn(Optional.of(mockPartEntity));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/part-entities/{id}", partEntityId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(partEntityId));
+    }
+
+    /*@Test
+    public void testCreatePartEntity() throws Exception {
+        PartEntities newPartEntity = new PartEntities();
+        when(partEntitiesRepository.save(newPartEntity)).thenReturn(newPartEntity);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/part-entities")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newPartEntity)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }*/
+}
---
+++ src/test/java/com/example/BackendExam/EndToEndTests/SubassemblyEndToEndTest.java
@@ -0,0 +1,68 @@
+package com.example.BackendExam.EndToEndTests;
+
+import com.example.BackendExam.model.Subassembly;
+import com.example.BackendExam.repository.SubassemblyRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SubassemblyEndToEndTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SubassemblyRepository subassemblyRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    /*@Test
+    public void testGetSubassemblies() throws Exception {
+        List<Subassembly> mockSubassemblies = List.of(new Subassembly(), new Subassembly());
+        when(subassemblyRepository.findAll()).thenReturn(mockSubassemblies);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/subassemblies"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(mockSubassemblies.size()));
+    }
+
+    @Test
+    public void testGetSubassemblyById() throws Exception {
+        Long subassemblyId = 1L;
+        Subassembly mockSubassembly = new Subassembly();
+        mockSubassembly.setId(subassemblyId);
+        when(subassemblyRepository.findById(subassemblyId)).thenReturn(Optional.of(mockSubassembly));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/subassemblies/{id}", subassemblyId))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(subassemblyId));
+    }
+
+    @Test
+    public void testCreateSubassembly() throws Exception {
+        Subassembly newSubassembly = new Subassembly();
+        when(subassemblyRepository.save(newSubassembly)).thenReturn(newSubassembly);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/subassemblies")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newSubassembly)))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }*/
+}
